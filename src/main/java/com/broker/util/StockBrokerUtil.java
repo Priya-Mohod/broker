63fb6139bc9d88d9610509e9bb72c79150b7a6c9
priya-mohod <contact.priyamohod@gmail.com>
2023-11-06 18:32:39 +0000

    creating war file

--- src/main/java/com/broker/util/StockBrokerUtil.java
+++ src/main/java/com/broker/util/StockBrokerUtil.java
@@ -10,28 +10,25 @@ import com.broker.repository.UserRepository;
 
 @Component
 public class StockBrokerUtil {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private CurrencyMetadataRepository currencyMetadataRepository;
-	
+
 	public Double getCurrencyValueForLoggedInUser() {
 		User user = userRepository.findById(1L).get();
 		CurrencyMetadata currencyMetadata = currencyMetadataRepository.findByCurrencyName(user.getCurrency());
 		return currencyMetadata.getCurrencyValue();
 	}
-	
+
 	public String getCurrencyNameForLoggedInUser() {
-		
-	return userRepository.findById(1L).get().getCurrency();
+
+		return userRepository.findById(1L).get().getCurrency();
 	}
-	
+
 	public Long getAvailableBalanceForLoggedInUser() {
 		return userRepository.findById(1L).get().getAccountBalance();
 	}
-	
-	
-	
 }
