10b5d204a853758cc4d8903e8d464f00f49a4328
Priya Mohod <96761705+Priya-Mohod@users.noreply.github.com>
2023-11-09 21:18:15 +0000

    Delete broker directory
    updating broker

--- broker/src/main/java/com/broker/repository/PortfolioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.broker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.broker.modal.Portfolio;
-
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-
-}
